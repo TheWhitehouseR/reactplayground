2db53af1bc2c7d43f1733e3fe03192c73306adc4
Ryan Whitehouse <thewhitehouser@gmail.com>
2016-08-14 15:33:01 +1200

    First commit

---
+++ src/components/app.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import TodosList from './todos-list';
+import CreateTodo from './create-todo';
+import _ from 'lodash';
+
+const todos = [
+  {
+      task: 'make React tutorial',
+      isCompleted: false,
+  },
+  {
+      task: 'eat dinner',
+      isCompleted: true,
+  }
+];
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos
+        }
+    }
+
+    createTask(task) {
+        this.state.todos.push({
+            task,
+            isCompleted: false,
+        });
+        this.setState({ todos: this.state.todos })
+    }
+
+    toggleTask(task) {
+        const existingTask = _.find(this.state.todos, todo => todo.task === task);
+        existingTask.isCompleted = !existingTask.isCompleted;
+        this.setState({ todos: this.state.todos })
+    }
+
+    saveTask(oldTask, newTask) {
+        const existingTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+        existingTodo.task = newTask;
+        this.setState({ todos: this.state.todos })
+    }
+
+    deleteTask(taskToDelete) {
+        _.remove(this.state.todos, todo => todo.task === taskToDelete);
+        this.setState({ todos: this.state.todos })
+    }
+
+  render() {
+    return (
+      <div>
+        <h1>React Todos App</h1>
+        <CreateTodo createTask={this.createTask.bind(this)} todos={this.state.todos} />
+        <TodosList todos={this.state.todos} 
+          toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}/>
+      </div>
+    );
+  }
+}
